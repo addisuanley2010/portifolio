b2e02f0b6ed397e9749c02162295b0e7a8a6c304
addisuanley2010 <addisuanley2005@gmail.com>
2022-11-28 00:02:36 +0300

    upload image to database

--- src/componenets/UploadPost.jsx
+++ src/componenets/UploadPost.jsx
@@ -17,8 +17,12 @@ import {
   TextareaAutosize,
   Divider,
 } from "@mui/material";
-
+import axios from "axios";
 const UploadPost = () => {
+  const [post, setPost] = useState({
+    title: "",
+    description: "",
+  });
   const [selectedImage, setSelectedImage] = useState(null);
   const [imageUrl, setImageUrl] = useState(null);
 
@@ -27,107 +31,143 @@ const UploadPost = () => {
       setImageUrl(URL.createObjectURL(selectedImage));
     }
   }, [selectedImage]);
+  console.log(imageUrl);
+
+const handleSubmit=()=>{
+  const data={
+    title:post.title,
+    description:post.description,
+    image:imageUrl
+  }
+  axios.post("http://localhost:3001/post", data).then((res) => {
+      if (res.data.error) {
+       console.log("error")
+      } else {
+console.log("yesssss!")
+      }
+    });
+}
+
 
   return (
-    <Box alignItems="center">
-    <Stack
-      margin={"10px"}
-      alignItems="center"
-      direction={"row"}
-      justifyContent="space-evenly"
-       
-      sx={{
-        flexDirection: {
-          sm: "column",
-          xs: "column",
-          md: "row",
-          lg: "row",
-          xl: "row",
-        },
-        minWidth: {
-          xs: "350px",
-          sm: "600px",
-        },
-        gap: "40px",
-      }}
-    >
-      <TextField
-        accept="image/*"
-        type="file"
-        id="select-image"
-        style={{ display: "none" }}
-        onChange={(e) => setSelectedImage(e.target.files[0])}
-      />
-      <Box
-        mt={"50px"}
-        display="flex"
-        flexDirection={"column"}
+    <Box>
+      <Stack
+        margin={"10px"}
+        alignItems="center"
+        direction={"row"}
+        justifyContent="space-evenly"
         sx={{
+          flexDirection: {
+            sm: "column",
+            xs: "column",
+            md: "row",
+            lg: "row",
+            xl: "row",
+          },
           minWidth: {
             xs: "350px",
             sm: "600px",
           },
-          gap: "40px",
+          gap: "10px",
         }}
       >
-        <TextField placeholder="Title" fullWidth />
-        <TextareaAutosize placeholder="Description" minRows={5} />
-        
-       
-      </Box>
-<Divider/>
-
-      {imageUrl && selectedImage && (
-        <Card sx={{ maxWidth: 700 }}>
-          <CardMedia
-            component="img"
-            width="300"
-            height="300"
-            image={imageUrl}
-            alt="adda"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              Addisu
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Lizards are a widespread group of squamate reptiles, with over
-              6,000 species, ranging across all continents except Antarctica
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <IconButton onClick={() => {}}>
-              <ThumbUpIcon />
-            </IconButton>
-            <FavoriteIcon />
-            <AddReactionIcon />
-            <Button size="small" color="primary">
-              Share
-            </Button>
-            <Button size="small" color="primary">
-              comment
-            </Button>
-          </CardActions>
-        </Card>
-      )}
-      <label htmlFor="select-image">
-          <Button variant="contained" color="primary" component="span">
-            <AddAPhotoIcon />
-          </Button>
-        </label>
-    </Stack> 
-    <Button
+        <TextField
+          accept="image/*"
+          type="file"
+          id="select-image"
+          style={{ display: "none" }}
+          onChange={(e) => setSelectedImage(e.target.files[0])}
+        />
+        <Box
+          mt={"50px"}
+          display="flex"
+          flexDirection={"column"}
           sx={{
-            marginTop: "15%",
-            marginLeft: "58%",
-            "&:hover": {
-              backgroundColor: "primary.light",
+            minWidth: {
+              xs: "350px",
+              sm: "600px",
             },
+            gap: "40px",
           }}
-          variant="outlined"
         >
-          <Button>Download Cv</Button>
-        </Button>
+          <TextField
+            value={post.title}
+            placeholder="Title"
+            fullWidth
+            onChange={(e) => {
+              setPost({ ...post, title: e.target.value });
+            }}
+          />
+          <TextareaAutosize
+            value={post.description}
+            placeholder="Description"
+            minRows={5}
+            onChange={(e) => {
+              setPost({ ...post, description: e.target.value });
+            }}
+          />
+        </Box>
+        <Divider />
+
+        {imageUrl && selectedImage && (
+          <Card sx={{ maxWidth: 700 }}>
+            <CardMedia
+              component="img"
+              width="300"
+              height="300"
+              image={imageUrl}
+              alt="adda"
+            />
+            <CardContent>
+              <label htmlFor="select-image">
+                <Button color="primary" component="span">
+                  <AddAPhotoIcon />
+                </Button>
+              </label>
+              <Typography gutterBottom variant="h5" component="div">
+                {post.title}
+              </Typography>
+
+              <Typography variant="body2" color="text.secondary">
+                {post.description}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <IconButton onClick={() => {}}>
+                <ThumbUpIcon />
+              </IconButton>
+              <FavoriteIcon />
+              <AddReactionIcon />
+              <Button size="small" color="primary">
+                Share
+              </Button>
+              <Button size="small" color="primary">
+                comment
+              </Button>
+            </CardActions>
+          </Card>
+        )}
+        {!selectedImage && (
+          <label htmlFor="select-image">
+            <Button variant="contained" color="primary" component="span">
+              <AddAPhotoIcon />
+            </Button>
+          </label>
+        )}
+      </Stack>
+      <Button
+        sx={{
+          marginTop: "15%",
+          marginLeft: "58%",
+          "&:hover": {
+            backgroundColor: "primary.light",
+          },
+        }}
+        variant="outlined"
+        onClick={handleSubmit}
+      >
+        Submit Post
+      </Button>
     </Box>
   );
 };
